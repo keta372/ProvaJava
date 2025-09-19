0089e5a04c32e58be47ddb02c221616590a8d623
Kevin_Tautu <keta372@vidalibarraquer.net>
2025-09-19 12:27:10 +0200

    He afegit un out

--- src/main/java/org/yourcompany/yourproject/ProvaJava.java
+++ src/main/java/org/yourcompany/yourproject/ProvaJava.java
@@ -11,6 +11,6 @@ package org.yourcompany.yourproject;
 public class ProvaJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hellyhfyhtfhfyjhgfjho World!");
     }
 }
